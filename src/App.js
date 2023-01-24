191db79bcb96e5e48bd2104585833da73ac1125d
femsworld <femsworld@gmail.com>
2023-01-24 09:53:42 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
 
+let name = 'Erik  ';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Navbar title= 'Practice project' aboutText= 'About Us'/> */}
+    <Navbar/>
+    <Textform/>
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+        <div className="container">
+        <div className="mb-3">
+        <h1 className="my-3">Example</h1>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+        </div>
+     
+    </div>
+  );
+}
