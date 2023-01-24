ebb817fd288e7e43d0cfaba302fc0ae588b760fc
femsworld <femsworld@gmail.com>
2023-01-24 13:57:43 +0200

    Added Switch to Navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import { useState } from 'react';
 
 let name = 'Erik  ';
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=> {
+    if (mode === 'light') {
+      setMode('dark')
+      
+    } else {
+      setMode('light')
+    }
+  }
+
   return (
     <>
     {/* <Navbar title= 'Practice project' aboutText= 'About Us'/> */}
-    <Navbar/>
-    {/* <Textform heading = 'Text Area'/> */}
-    <About/>
+    <Navbar mode={mode} toggleMode={toggleMode}/>
+    <Textform heading = 'Text Area'/>
+    {/* <About/> */}
     </>
   );
 }
